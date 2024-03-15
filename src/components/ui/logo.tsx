858f23c7d69e174cf2c7ebe64489e8220f912ecd
Kinetic639 <michal.stepien36@gmail.com>
2024-03-15 13:00:28 +0100

    added basic colors, fixed font

--- src/components/ui/logo.tsx
+++ src/components/ui/logo.tsx
@@ -1,8 +1,11 @@
 import React from "react";
-
+import Image from "next/image";
 
 export const Logo = () => {
-    return (
-      <div>image logo</div>
-    );
+	return (
+		<div className="relative flex p-2">
+			<span className="absolute bottom-0 left-0 z-[-1] block h-[200%] w-full rounded-b-[40px] bg-accent"></span>
+			<Image src="/assets/images/brand_logo.png" width={80} height={72} alt="logo picture" />
+		</div>
+	);
 };
