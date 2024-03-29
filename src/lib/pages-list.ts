55b3d7554ed0faa7042579291f9ccffb1283a62c
Kinetic639 <michal.stepien36@gmail.com>
2024-03-30 00:00:02 +0100

    Added content to our specialists section

--- src/lib/pages-list.ts
+++ src/lib/pages-list.ts
@@ -2,7 +2,6 @@ export const pages = [
 	{ title: "główna", href: "/" },
 	{ title: "o nas", href: "/specjalisci" },
 	{ title: "usługi", href: "/uslugi" },
-	{ title: "cennik", href: "/cennik" },
 	{ title: "Newsy", href: "/artykuly" },
 	{ title: "produkty", href: "/produkty" },
 ];
